701ec7f4dc25e938edbd560a20387def8a29d1d3
Emil Fahlgren <emil.fahlgren@gmail.com>
2024-05-17 12:29:20 +0200

    Initial synth commit

---
+++ Synth/PluginEditor.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "PluginProcessor.h"
+
+//==============================================================================
+class SynthAudioProcessorEditor final : public juce::AudioProcessorEditor
+{
+public:
+    explicit SynthAudioProcessorEditor (SynthAudioProcessor&);
+    ~SynthAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    SynthAudioProcessor& processorRef;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SynthAudioProcessorEditor)
+};
