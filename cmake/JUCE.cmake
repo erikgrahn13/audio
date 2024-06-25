8116bd6433e24fa0c6503a7009a16162ee437777
singularity <erikgrahn13@gmail.com>
2024-06-25 15:32:17 +0200

    update juce to version 8.0.0 (#17)

--- cmake/JUCE.cmake
+++ cmake/JUCE.cmake
@@ -2,7 +2,7 @@ add_compile_definitions(JUCE_DISPLAY_SPLASH_SCREEN=0)
 FetchContent_Declare(
   juce
   GIT_REPOSITORY https://github.com/juce-framework/JUCE.git
-  GIT_TAG 7.0.12
+  GIT_TAG 8.0.0
   SOURCE_DIR ${DEPENDENCY_DIR}/juce
 )
 
