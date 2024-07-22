1b93e83f4ddc9bdc03a9f32d105716fc66850073
Erik Grahn <erikgrahn13@gmail.com>
2024-07-22 20:50:10 +0200

    remove CI_BUILD and use JUCE_BUILD_EXTRAS

--- cmake/Pluginval.cmake
+++ cmake/Pluginval.cmake
@@ -4,12 +4,10 @@ function(enable_pluginval_testing target)
     add_test(NAME ${target}_VST3_TEST 
              COMMAND ${pluginval_SOURCE_DIR}/${PLUGINVAL_BINARY_PATH} --strictness-level 10 --verbose --skip-gui-tests --validate-in-process ${PLUGIN_PATH}/VST3/${target}.vst3)
 
-    option(CI_BUILD "Enables pluginval tests of AU plugins" OFF)
-
-    if(APPLE AND CI_BUILD)
+    if(APPLE AND NOT JUCE_BUILD_EXTRAS)
         add_test(NAME ${target}_AU_TEST 
         COMMAND ${pluginval_SOURCE_DIR}/${PLUGINVAL_BINARY_PATH} --strictness-level 10 --verbose --skip-gui-tests --validate-in-process $ENV{HOME}/Library/Audio/Plug-Ins/Components/${target}.component)
-    endif(APPLE AND CI_BUILD)
+    endif(APPLE AND NOT JUCE_BUILD_EXTRAS)
 
 endfunction(enable_pluginval_testing)
 
