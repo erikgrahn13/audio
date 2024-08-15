75f505ba24d103e828bfd5a17de55e7a7dd8f1c7
Erik Grahn <erikgrahn13@gmail.com>
2024-08-15 10:48:35 +0200

    creating test step github actions

--- cmake/Pluginval.cmake
+++ cmake/Pluginval.cmake
@@ -5,21 +5,11 @@ function(enable_pluginval_testing target)
              COMMAND ${pluginval_SOURCE_DIR}/${PLUGINVAL_BINARY_PATH} --strictness-level 10 --verbose --skip-gui-tests --validate-in-process ${PLUGIN_PATH}/VST3/${target}.vst3)
 
     if(APPLE AND NOT JUCE_BUILD_EXTRAS)
-        # Make the post_build_command target depend on all your other targets
-        add_dependencies(post_build_command ${target})
         add_test(NAME ${target}_AU_TEST 
         COMMAND ${pluginval_SOURCE_DIR}/${PLUGINVAL_BINARY_PATH} --strictness-level 10 --verbose --skip-gui-tests --validate-in-process $ENV{HOME}/Library/Audio/Plug-Ins/Components/${target}.component)
     endif(APPLE AND NOT JUCE_BUILD_EXTRAS)
 
 endfunction(enable_pluginval_testing)
-
-# Create a custom target for restarting the AudioComponentRegistrar to be able to register AU plugins to the system
-if(APPLE AND NOT JUCE_BUILD_EXTRAS)
-    add_custom_target(
-        post_build_command ALL
-        COMMAND pgrep -x AudioComponentRegistrar >/dev/null && killall -9 AudioComponentRegistrar; ${CMAKE_COMMAND} -E echo "killed AudioComponentRegistrar" || ${CMAKE_COMMAND} -E echo "AudioComponentRegistrar Process not found"
-    )
-endif()
 
 if(WIN32)
     set(PLUGINVAL_URL "https://github.com/Tracktion/pluginval/releases/latest/download/pluginval_Windows.zip")
