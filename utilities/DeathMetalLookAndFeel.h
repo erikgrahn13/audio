b05bf7f02b0520dcc197f7e1ef2e9bfbd621991a
singularity <erikgrahn13@gmail.com>
2024-10-06 17:10:20 +0200

    make custom slider with pentagram as handle (#67)

--- utilities/DeathMetalLookAndFeel.h
+++ utilities/DeathMetalLookAndFeel.h
@@ -30,6 +30,151 @@ class DeathMetalLookAndFeel : public juce::LookAndFeel_V4
         button.setColour(juce::TextButton::ColourIds::buttonOnColourId, juce::Colours::black);
     }
 
+    void drawLinearSlider(Graphics &g, int x, int y, int width, int height, float sliderPos, float minSliderPos,
+                          float maxSliderPos, Slider::SliderStyle style, Slider &slider) override
+    {
+        if (slider.isBar())
+        {
+            g.setColour(slider.findColour(Slider::trackColourId));
+            g.fillRect(slider.isHorizontal() ? Rectangle<float>(static_cast<float>(x), (float)y + 0.5f,
+                                                                sliderPos - (float)x, (float)height - 1.0f)
+                                             : Rectangle<float>((float)x + 0.5f, sliderPos, (float)width - 1.0f,
+                                                                (float)y + ((float)height - sliderPos)));
+
+            drawLinearSliderOutline(g, x, y, width, height, style, slider);
+        }
+        else
+        {
+            auto isTwoVal =
+                (style == Slider::SliderStyle::TwoValueVertical || style == Slider::SliderStyle::TwoValueHorizontal);
+            auto isThreeVal = (style == Slider::SliderStyle::ThreeValueVertical ||
+                               style == Slider::SliderStyle::ThreeValueHorizontal);
+
+            auto trackWidth = jmin(6.0f, slider.isHorizontal() ? (float)height * 0.25f : (float)width * 0.25f);
+
+            Point<float> startPoint(slider.isHorizontal() ? (float)x : (float)x + (float)width * 0.5f,
+                                    slider.isHorizontal() ? (float)y + (float)height * 0.5f : (float)(height + y));
+
+            Point<float> endPoint(slider.isHorizontal() ? (float)(width + x) : startPoint.x,
+                                  slider.isHorizontal() ? startPoint.y : (float)y);
+
+            Path backgroundTrack;
+            backgroundTrack.startNewSubPath(startPoint);
+            backgroundTrack.lineTo(endPoint);
+            g.setColour(slider.findColour(Slider::backgroundColourId));
+            g.strokePath(backgroundTrack, {trackWidth, PathStrokeType::curved, PathStrokeType::rounded});
+
+            Path valueTrack;
+            Point<float> minPoint, maxPoint, thumbPoint;
+
+            if (isTwoVal || isThreeVal)
+            {
+                minPoint = {slider.isHorizontal() ? minSliderPos : (float)width * 0.5f,
+                            slider.isHorizontal() ? (float)height * 0.5f : minSliderPos};
+
+                if (isThreeVal)
+                    thumbPoint = {slider.isHorizontal() ? sliderPos : (float)width * 0.5f,
+                                  slider.isHorizontal() ? (float)height * 0.5f : sliderPos};
+
+                maxPoint = {slider.isHorizontal() ? maxSliderPos : (float)width * 0.5f,
+                            slider.isHorizontal() ? (float)height * 0.5f : maxSliderPos};
+            }
+            else
+            {
+                auto kx = slider.isHorizontal() ? sliderPos : ((float)x + (float)width * 0.5f);
+                auto ky = slider.isHorizontal() ? ((float)y + (float)height * 0.5f) : sliderPos;
+
+                minPoint = startPoint;
+                maxPoint = {kx, ky};
+            }
+
+            auto thumbWidth = getSliderThumbRadius(slider) + 4;
+
+            valueTrack.startNewSubPath(minPoint);
+            valueTrack.lineTo(isThreeVal ? thumbPoint : maxPoint);
+            g.setColour(slider.findColour(Slider::trackColourId));
+            g.strokePath(valueTrack, {trackWidth, PathStrokeType::curved, PathStrokeType::rounded});
+
+            if (!isTwoVal)
+            {
+                g.setColour(juce::Colours::black);
+                g.fillEllipse(Rectangle<float>(static_cast<float>(thumbWidth), static_cast<float>(thumbWidth))
+                                  .withCentre(isThreeVal ? thumbPoint : maxPoint));
+
+                g.setColour(juce::Colours::white);
+
+                juce::Point<float> point1(maxPoint.getX(), maxPoint.getY());
+                point1.setX(point1.getX() + (thumbWidth / 2) * ::sinf(0.f * std::numbers::pi / 180.f));
+                point1.setY(point1.getY() + (thumbWidth / 2) * ::cosf(0.f * std::numbers::pi / 180.f));
+
+                juce::Point<float> point2(maxPoint.getX(), maxPoint.getY());
+                point2.setX(point2.getX() +
+                            (thumbWidth / 2) * ::sinf(144.f * static_cast<float>(std::numbers::pi) / 180.f));
+                point2.setY(point2.getY() +
+                            (thumbWidth / 2) * ::cosf(144.f * static_cast<float>(std::numbers::pi) / 180.f));
+
+                juce::Point<float> point3(maxPoint.getX(), maxPoint.getY());
+                point3.setX(point3.getX() +
+                            (thumbWidth / 2) * ::sinf(288.f * static_cast<float>(std::numbers::pi) / 180.f));
+                point3.setY(point3.getY() +
+                            (thumbWidth / 2) * ::cosf(288.f * static_cast<float>(std::numbers::pi) / 180.f));
+
+                juce::Point<float> point4(maxPoint.getX(), maxPoint.getY());
+                point4.setX(point4.getX() +
+                            (thumbWidth / 2) * ::sinf(72.f * static_cast<float>(std::numbers::pi) / 180.f));
+                point4.setY(point4.getY() +
+                            (thumbWidth / 2) * ::cosf(72.f * static_cast<float>(std::numbers::pi) / 180.f));
+
+                juce::Point<float> point5(maxPoint.getX(), maxPoint.getY());
+                point5.setX(point5.getX() +
+                            (thumbWidth / 2) * ::sinf(216.f * static_cast<float>(std::numbers::pi) / 180.f));
+                point5.setY(point5.getY() +
+                            (thumbWidth / 2) * ::cosf(216.f * static_cast<float>(std::numbers::pi) / 180.f));
+
+                juce::Path pentagramPath;
+                pentagramPath.startNewSubPath(point1);
+                pentagramPath.lineTo(point2);
+                pentagramPath.lineTo(point3);
+                pentagramPath.lineTo(point4);
+                pentagramPath.lineTo(point5);
+                pentagramPath.closeSubPath();
+                g.strokePath(pentagramPath, juce::PathStrokeType(1));
+
+                g.drawEllipse(Rectangle<float>(static_cast<float>(thumbWidth), static_cast<float>(thumbWidth))
+                                  .withCentre(isThreeVal ? thumbPoint : maxPoint)
+                                  .reduced(2, 2),
+                              1.);
+            }
+
+            if (isTwoVal || isThreeVal)
+            {
+                auto sr = jmin(trackWidth, (slider.isHorizontal() ? (float)height : (float)width) * 0.4f);
+                auto pointerColour = slider.findColour(Slider::thumbColourId);
+
+                if (slider.isHorizontal())
+                {
+                    drawPointer(g, minSliderPos - sr, jmax(0.0f, (float)y + (float)height * 0.5f - trackWidth * 2.0f),
+                                trackWidth * 2.0f, pointerColour, 2);
+
+                    drawPointer(g, maxSliderPos - trackWidth,
+                                jmin((float)(y + height) - trackWidth * 2.0f, (float)y + (float)height * 0.5f),
+                                trackWidth * 2.0f, pointerColour, 4);
+                }
+                else
+                {
+                    drawPointer(g, jmax(0.0f, (float)x + (float)width * 0.5f - trackWidth * 2.0f),
+                                minSliderPos - trackWidth, trackWidth * 2.0f, pointerColour, 1);
+
+                    drawPointer(g, jmin((float)(x + width) - trackWidth * 2.0f, (float)x + (float)width * 0.5f),
+                                maxSliderPos - sr, trackWidth * 2.0f, pointerColour, 3);
+                }
+            }
+
+            if (slider.isBar())
+                drawLinearSliderOutline(g, x, y, width, height, style, slider);
+        }
+    }
+
     void drawRotarySlider(juce::Graphics &g, int x, int y, int width, int height, float sliderPosProportional,
                           float rotaryStartAngle, float rotaryEndAngle, juce::Slider &slider) override
     {
