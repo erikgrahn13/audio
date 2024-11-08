29b64040c58668325cda0290edcbec933b6fa447
erikgrahn13 <erikgrahn13@users.noreply.github.com>
2024-11-08 14:23:02 +0000

    deploy: cb01692001c2cc2b1c4abe2fffe03ca4cdca5590

--- search/all_1.js
+++ search/all_1.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['biquad_0',['Biquad',['../classBiquad.html',1,'']]],
-  ['blockquote_1',['&lt;/blockquote&gt;&lt;/blockquote&gt;',['../index.html#autotoc_md7',1,'']]],
-  ['blockquote_20blockquote_2',['&lt;/blockquote&gt;&lt;/blockquote&gt;',['../index.html#autotoc_md7',1,'']]],
-  ['build_3',['Build',['../index.html#autotoc_md6',1,'']]]
+  ['blockquote_1',['&lt;/blockquote&gt;',['../index.html#autotoc_md7',1,'']]],
+  ['build_2',['Build',['../index.html#autotoc_md6',1,'']]]
 ];
