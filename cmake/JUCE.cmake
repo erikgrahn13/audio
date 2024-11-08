e3a9810d75b3daae678fef671cde08f9acf5e2a0
singularity <erikgrahn13@gmail.com>
2024-11-08 15:21:53 +0100

    upgrade juce to 8.0.3 (#80)

--- cmake/JUCE.cmake
+++ cmake/JUCE.cmake
@@ -2,11 +2,11 @@
 
 
 if(WIN32)
-  set(JUCE_URL "https://github.com/juce-framework/JUCE/releases/download/8.0.2/juce-8.0.2-windows.zip")
+  set(JUCE_URL "https://github.com/juce-framework/JUCE/releases/download/8.0.3/juce-8.0.3-windows.zip")
 elseif(APPLE)
-  set(JUCE_URL "https://github.com/juce-framework/JUCE/releases/download/8.0.2/juce-8.0.2-osx.zip")
+  set(JUCE_URL "https://github.com/juce-framework/JUCE/releases/download/8.0.3/juce-8.0.3-osx.zip")
 elseif(UNIX)
-  set(JUCE_URL "https://github.com/juce-framework/JUCE/releases/download/8.0.2/juce-8.0.2-linux.zip")
+  set(JUCE_URL "https://github.com/juce-framework/JUCE/releases/download/8.0.3/juce-8.0.3-linux.zip")
 else()
   message(FATAL_ERROR "Not supported platform")
 endif()
