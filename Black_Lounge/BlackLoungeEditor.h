f1f1c4851853b6b03d0f0fdee43afcf60c78237d
Erik Grahn <erikgrahn13@gmail.com>
2024-11-09 10:39:03 +0100

    enable gain parameter

--- Black_Lounge/BlackLoungeEditor.h
+++ Black_Lounge/BlackLoungeEditor.h
@@ -3,6 +3,7 @@
 #include "AmpView.h"
 #include "BlackLoungeProcessor.h"
 #include "CustomLookAndFeel/CustomStandaloneFilterWindow.h"
+#include "CustomLookAndFeel/DeathMetalLookAndFeel.h"
 #include "CustomLookAndFeel/FontWebLookAndFeel.h"
 #include "SettingsView.h"
 #include <JuceHeader.h>
@@ -22,13 +23,22 @@ class BlackLoungeAudioProcessorEditor final : public juce::AudioProcessorEditor
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     BlackLoungeAudioProcessor &processorRef;
-    // std::unique_ptr<juce::AudioDeviceSelectorComponent> audioSetupComp;
-    // juce::AudioDeviceManager audioDeviceManager;
     juce::TextButton button;
+    juce::Label volumeLabel;
+    juce::Slider volumeSlider;
+    juce::Label thresholdLabel;
+    juce::Slider thresholdSlider;
+    juce::Label gainLabel;
+    juce::Slider gainSlider;
     juce::TabbedComponent mainScreens{juce::TabbedButtonBar::TabsAtBottom};
     AmpView ampView;
     SettingsView settingsView;
     FontWebLookAndFeel fontWebLookAndFeel;
+    DeathMetalLookAndFeel fontDeathMetalLookAndFeel;
+
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> mVolumeAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> mThresholdAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> mGainAttachment;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(BlackLoungeAudioProcessorEditor)
 };
