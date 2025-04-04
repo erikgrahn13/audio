931015e6308fc9e9a8f67cfa21fa16018e5191ae
Erik Grahn <erikgrahn13@gmail.com>
2025-04-04 12:20:39 +0200

    fix failing test of black lounge

--- cmake/Pluginval.cmake
+++ cmake/Pluginval.cmake
@@ -1,14 +1,14 @@
 function(enable_pluginval_testing target)
     get_target_property(PLUGIN_PATH ${target} LIBRARY_OUTPUT_DIRECTORY)
+    get_target_property(PLUGIN_NAME ${PROJECT_NAME} JUCE_PRODUCT_NAME)
 
-    add_test(NAME ${target}_VST3_TEST 
-             COMMAND ${pluginval_SOURCE_DIR}/${PLUGINVAL_BINARY_PATH} --strictness-level 10 --verbose --skip-gui-tests --validate-in-process ${PLUGIN_PATH}/VST3/${target}.vst3)
+    add_test(NAME ${target}_VST3_TEST
+        COMMAND ${pluginval_SOURCE_DIR}/${PLUGINVAL_BINARY_PATH} --strictness-level 10 --verbose --skip-gui-tests --validate-in-process ${PLUGIN_PATH}/VST3/${PLUGIN_NAME}.vst3)
 
     if(APPLE AND NOT JUCE_BUILD_EXTRAS)
-        add_test(NAME ${target}_AU_TEST 
-        COMMAND ${pluginval_SOURCE_DIR}/${PLUGINVAL_BINARY_PATH} --strictness-level 10 --verbose --skip-gui-tests --validate-in-process $ENV{HOME}/Library/Audio/Plug-Ins/Components/${target}.component)
+        add_test(NAME ${target}_AU_TEST
+            COMMAND ${pluginval_SOURCE_DIR}/${PLUGINVAL_BINARY_PATH} --strictness-level 10 --verbose --skip-gui-tests --validate-in-process $ENV{HOME}/Library/Audio/Plug-Ins/Components/${PLUGIN_NAME}.component)
     endif(APPLE AND NOT JUCE_BUILD_EXTRAS)
-
 endfunction(enable_pluginval_testing)
 
 if(WIN32)
@@ -23,8 +23,8 @@ elseif(UNIX)
 endif()
 
 FetchContent_Declare(
-  pluginval
-  URL ${PLUGINVAL_URL}
-  DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+    pluginval
+    URL ${PLUGINVAL_URL}
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
 )
 FetchContent_MakeAvailable(pluginval)
