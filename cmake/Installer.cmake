8ecce5312c2dc3ef9298236cf86b0a4e25a55be2
singularity <erikgrahn13@gmail.com>
2025-08-04 17:42:36 +0200

    Installer-mac
    * add state handling to every plugin
    update manufacturer code
    
    * create mac installers and install plugins in github actions
    
    * fix so windows should work
    
    * include linux
    
    * add path to linux
    
    * update the workflow preset to only include black louge
    
    * update testing
    
    * correct plugin name in tests
    
    * temporary change to be able to test if installing works
    
    * trye to check is plugin is installed and in case run test from the installed version
    
    * try install plugins at default place as well
    
    * try fix testing
    
    * correct path of vst3 for testing
    
    * try improve testing
    
    * fix test again
    
    * change hardcoded paths for cmake_install
    
    * try to include all ninja files
    
    * try to merge build and test step in github actions
    
    * fix testing for all OS
    
    * clean up workflow file
    
    * updating ci build
    
    * update even more
    
    * improve even more
    
    * redo

---
+++ cmake/Installer.cmake
@@ -0,0 +1,77 @@
+function(create_installer target)
+    get_target_property(PLUGIN_IS_COPIED ${target} JUCE_COPY_PLUGIN_AFTER_BUILD)
+
+    if(PLUGIN_IS_COPIED)
+        message(FATAL_ERROR "Plugin ${target} cant have COPY_PLUGIN_AFTER_BUILD set to TRUE while also call create_installer CMake function")
+    endif()
+
+    get_target_property(PLUGIN_NAME ${PROJECT_NAME} JUCE_PRODUCT_NAME)
+    get_target_property(BUNDLE_ID ${target} JUCE_BUNDLE_ID)
+
+    set(CPACK_OUTPUT_CONFIG_FILE "${CMAKE_CURRENT_BINARY_DIR}/CPackConfig-${target}.cmake")
+    set(CPACK_PACKAGE_NAME "${PLUGIN_NAME}")
+    set(CPACK_PACKAGE_VERSION "1.0.0")
+    set(CPACK_PRODUCTBUILD_IDENTIFIER "${BUNDLE_ID}")
+    set(CPACK_PACKAGE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
+    set(CPACK_SYSTEM_NAME "")
+    set(CPACK_PACKAGE_FILE_NAME "${target}-${CPACK_PACKAGE_VERSION}")
+
+    if(APPLE)
+        set(APP_INSTALL_DIRECTORY "Applications")
+        set(VST3_INSTALL_DIRECTORY "Library/Audio/Plug-Ins/VST3")
+    elseif(WIN32) # fix correct destinations when inno setup support is added
+        set(APP_INSTALL_DIRECTORY "tmp")
+        set(VST3_INSTALL_DIRECTORY "tmp")
+    elseif(UNIX)
+        set(APP_INSTALL_DIRECTORY "tmp")
+        set(VST3_INSTALL_DIRECTORY "tmp")
+    endif()
+
+    install(TARGETS ${target}_VST3
+        DESTINATION ${VST3_INSTALL_DIRECTORY}
+        COMPONENT ${target}VST3
+    )
+
+    install(TARGETS ${target}_Standalone
+        DESTINATION ${APP_INSTALL_DIRECTORY}
+        COMPONENT ${target}APP
+    )
+
+    set(CPACK_COMPONENTS_ALL ${target}APP ${target}VST3)
+    set(CPACK_COMPONENT_NAME "${target}")
+
+    # Mac installer
+    if(APPLE)
+        include(CPackComponent)
+        set(_entries "")
+        string(APPEND _entries
+            "  <dict>
+            <key>RootRelativeBundlePath</key><string>Applications/${PLUGIN_NAME}.app</string>
+            <key>BundleIsRelocatable</key><false/>
+            <key>BundleHasStrictIdentifier</key><true/>
+            <key>BundleIsVersionChecked</key><true/>
+        </dict>
+        ")
+
+        configure_file("${CMAKE_SOURCE_DIR}/cmake/component.plist.in" "component.plist" @ONLY)
+        cpack_add_component(${target}APP PLIST "${CMAKE_CURRENT_BINARY_DIR}/component.plist")
+
+        install(TARGETS ${target}_AU
+            DESTINATION "Library/Audio/Plug-Ins/Components"
+            COMPONENT ${target}AU
+        )
+        list(APPEND CPACK_COMPONENTS_ALL ${target}AU)
+
+        set(CPACK_COMPONENT_${target}AU_DISPLAY_NAME "${PLUGIN_NAME} AU")
+
+        set(CPACK_PACKAGING_INSTALL_PREFIX "/")
+        set(CPACK_GENERATOR "productbuild")
+
+    # Windows installer
+    else()
+        set(CPACK_GENERATOR "TGZ")
+    endif()
+
+    # Linux installer
+    include(CPack)
+endfunction()
--- cmake/Pluginval.cmake
+++ cmake/Pluginval.cmake
@@ -8,15 +8,31 @@ function(enable_pluginval_testing target)
     message(STATUS "✅ ${target}: ${bundle_id}")
 
     # Pluginval testing
-    get_target_property(PLUGIN_PATH ${target} LIBRARY_OUTPUT_DIRECTORY)
     get_target_property(PLUGIN_NAME ${PROJECT_NAME} JUCE_PRODUCT_NAME)
 
+    get_target_property(PLUGIN_PATH_VST3 ${target} JUCE_VST3_COPY_DIR)
+    get_target_property(PLUGIN_PATH_AU ${target} JUCE_AU_COPY_DIR)
+    get_target_property(PLUGIN_IS_COPIED ${target} JUCE_COPY_PLUGIN_AFTER_BUILD)
+
+    if(NOT PLUGIN_IS_COPIED)
+        if(APPLE)
+            set(PLUGIN_PATH_AU "/Library/Audio/Plug-Ins/Components")
+            set(PLUGIN_PATH_VST3 "/Library/Audio/Plug-Ins/VST3")
+        elseif(WIN32) # TODO: this is temporary until an installer for windows is in place
+            get_target_property(PLUGIN_PATH ${target} LIBRARY_OUTPUT_DIRECTORY)
+            set(PLUGIN_PATH_VST3 "${PLUGIN_PATH}/VST3")
+        elseif(UNIX) # TODO: this is temporary until an installer for liunux is in place
+            get_target_property(PLUGIN_PATH ${target} LIBRARY_OUTPUT_DIRECTORY)
+            set(PLUGIN_PATH_VST3 "${PLUGIN_PATH}/VST3")
+        endif()
+    endif()
+
     add_test(NAME ${target}_VST3_TEST
-        COMMAND ${pluginval_SOURCE_DIR}/${PLUGINVAL_BINARY_PATH} --strictness-level 10 --verbose --skip-gui-tests --validate-in-process ${PLUGIN_PATH}/VST3/${PLUGIN_NAME}.vst3)
+        COMMAND ${pluginval_SOURCE_DIR}/${PLUGINVAL_BINARY_PATH} --strictness-level 10 --verbose --skip-gui-tests --validate-in-process ${PLUGIN_PATH_VST3}/${PLUGIN_NAME}.vst3)
 
     if(APPLE AND NOT JUCE_BUILD_EXTRAS)
         add_test(NAME ${target}_AU_TEST
-            COMMAND ${pluginval_SOURCE_DIR}/${PLUGINVAL_BINARY_PATH} --strictness-level 10 --verbose --skip-gui-tests --validate-in-process $ENV{HOME}/Library/Audio/Plug-Ins/Components/${PLUGIN_NAME}.component)
+            COMMAND ${pluginval_SOURCE_DIR}/${PLUGINVAL_BINARY_PATH} --strictness-level 10 --verbose --skip-gui-tests --validate-in-process ${PLUGIN_PATH_AU}/${PLUGIN_NAME}.component)
     endif(APPLE AND NOT JUCE_BUILD_EXTRAS)
 endfunction(enable_pluginval_testing)
 
