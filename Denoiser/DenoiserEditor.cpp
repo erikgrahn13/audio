d8e364e15bfa6c7baf43a85186d0e274d7ced4b5
Erik Grahn <erikgrahn13@gmail.com>
2026-01-10 16:16:55 +0100

    adding embryo to Denoiser plugin

---
+++ Denoiser/DenoiserEditor.cpp
@@ -0,0 +1,33 @@
+#include "DenoiserProcessor.h"
+#include "DenoiserEditor.h"
+
+//==============================================================================
+DenoiserAudioProcessorEditor::DenoiserAudioProcessorEditor (DenoiserAudioProcessor& p)
+    : AudioProcessorEditor (&p), processorRef (p)
+{
+    juce::ignoreUnused (processorRef);
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+}
+
+DenoiserAudioProcessorEditor::~DenoiserAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void DenoiserAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (juce::Colours::black);
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void DenoiserAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
---
+++ Denoiser/DenoiserEditor.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "DenoiserProcessor.h"
+#include "CustomLookAndFeel/DeathMetalLookAndFeel.h"
+//==============================================================================
+class DenoiserAudioProcessorEditor final : public juce::AudioProcessorEditor
+{
+public:
+    explicit DenoiserAudioProcessorEditor (DenoiserAudioProcessor&);
+    ~DenoiserAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    DenoiserAudioProcessor& processorRef;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DenoiserAudioProcessorEditor)
+};
