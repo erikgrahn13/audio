daaa1408fbaa3f66cc0fed4a668c86a10535fdd7
Erik Grahn <erikgrahn13@gmail.com>
2026-01-15 22:17:31 +0100

    design gui of Denoiser plugin

--- Denoiser/DenoiserEditor.cpp
+++ Denoiser/DenoiserEditor.cpp
@@ -1,5 +1,6 @@
 #include "DenoiserProcessor.h"
 #include "DenoiserEditor.h"
+#include "DenoiserBackgroundImage.h"
 
 //==============================================================================
 DenoiserAudioProcessorEditor::DenoiserAudioProcessorEditor (DenoiserAudioProcessor& p)
@@ -8,6 +9,30 @@ DenoiserAudioProcessorEditor::DenoiserAudioProcessorEditor (DenoiserAudioProcess
     juce::ignoreUnused (processorRef);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
+
+    auto imageInputStream = juce::MemoryInputStream(Denoiser::background_png, Denoiser::background_pngSize, false);
+    backgroundImage = juce::ImageFileFormat::loadFrom(imageInputStream);
+
+    titleLabel.setText("DENOISER", juce::NotificationType::dontSendNotification);
+    titleLabel.setJustificationType(juce::Justification::centred);
+    titleLabel.setLookAndFeel(&deathMetalLookAndFeel);
+    // auto font = titleLabel.getFont();
+    // font.setHeight(32.0f);
+    // titleLabel.setFont(font);
+    titleLabel.setFont(deathMetalLookAndFeel.getLabelFont(titleLabel).withHeight(64.0f));
+
+    // addAndMakeVisible(titleLabel);
+
+    mDenoiserAttachment.reset(new juce::AudioProcessorValueTreeState::SliderAttachment(
+        p.mParameters, "denoiser", denoiserSlider
+    ));
+    denoiserSlider.setLookAndFeel(&deathMetalLookAndFeel);
+    denoiserSlider.setSliderStyle(juce::Slider::RotaryVerticalDrag);
+    denoiserSlider.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
+    // denoiserSlider.setSize(10, 10);
+
+    addAndMakeVisible(denoiserSlider);
+
     setSize (400, 300);
 }
 
@@ -21,13 +46,33 @@ void DenoiserAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (juce::Colours::black);
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    if(backgroundImage.isValid())
+    {
+        // auto imageArea = getLo
+        g.drawImage(backgroundImage, getLocalBounds().reduced(20).toFloat());
+    }
+
+    g.setFont(deathMetalLookAndFeel.getLabelFont(titleLabel).withHeight(32.0f));
+    g.setColour(juce::Colours::white);
+    auto titleLabelBounds = getLocalBounds().removeFromTop(getLocalBounds().getHeight() / 4);
+    g.drawFittedText("DENOISER", titleLabelBounds, juce::Justification::centred, 1);
+
+    
+    // g.setFont(deathMetalLookAndFeel.getLabelFont(titleLabel).withHeight(32.0f));
+    // g.setColour(juce::Colours::white);
+    // g.drawFittedText("DENOISER", titleLabelBounds.reduced(20), juce::Justification::centredTop, 1);
 }
 
 void DenoiserAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    auto bounds = getLocalBounds();
+
+    auto titleArea = bounds.removeFromTop(bounds.getHeight() / 4);
+    titleLabel.setBounds(titleArea);
+
+
+    auto sliderArea = bounds.removeFromBottom(bounds.getHeight() / 3);
+    denoiserSlider.setBounds(sliderArea);
 }
--- Denoiser/DenoiserEditor.h
+++ Denoiser/DenoiserEditor.h
@@ -16,6 +16,11 @@ public:
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
+    juce::Label titleLabel;
+    juce::Image backgroundImage;
+    juce::Slider denoiserSlider;
+    DeathMetalLookAndFeel deathMetalLookAndFeel;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> mDenoiserAttachment;
     DenoiserAudioProcessor& processorRef;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DenoiserAudioProcessorEditor)
