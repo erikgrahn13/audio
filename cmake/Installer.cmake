a922f756ac3cef2c245c23dafeba9e2d595420a0
singularity <erikgrahn13@gmail.com>
2026-01-17 14:37:59 +0100

    Denoiser installer windows and improve installers (#129)

--- cmake/Installer.cmake
+++ cmake/Installer.cmake
@@ -28,64 +28,71 @@ function(create_installer target INSTALL_IMAGE)
         set(VST3_INSTALL_DIRECTORY ".")
     endif()
 
-    get_target_property(VST3_PATH ${target}_VST3 JUCE_PLUGIN_ARTEFACT_FILE)
-    get_target_property(APP_PATH ${target}_Standalone JUCE_PLUGIN_ARTEFACT_FILE)
+    set(CPACK_COMPONENTS_ALL)
+    include(CPackComponent)
 
-    install(DIRECTORY ${VST3_PATH}
-        DESTINATION ${VST3_INSTALL_DIRECTORY}
-        COMPONENT ${target}VST3
-        PATTERN "Plugin.ico" EXCLUDE
-    )
+    if(TARGET ${target}_VST3)
+        get_target_property(VST3_PATH ${target}_VST3 JUCE_PLUGIN_ARTEFACT_FILE)
+        install(DIRECTORY ${VST3_PATH}
+            DESTINATION ${VST3_INSTALL_DIRECTORY}
+            COMPONENT ${target}VST3
+            PATTERN "Plugin.ico" EXCLUDE
+        )
+        list(APPEND CPACK_COMPONENTS_ALL ${target}VST3)
+        cpack_add_component(${target}VST3
+            DISPLAY_NAME "${PLUGIN_NAME} VST3"
+            INSTALL_TYPES Full
+        )
+    endif()
+    
+    if(TARGET ${target}_Standalone)
+        get_target_property(APP_PATH ${target}_Standalone JUCE_PLUGIN_ARTEFACT_FILE)
+        install(PROGRAMS ${APP_PATH}
+            DESTINATION ${APP_INSTALL_DIRECTORY}
+            COMPONENT ${target}APP
+        )
+        list(APPEND CPACK_COMPONENTS_ALL ${target}APP)
+        cpack_add_component(${target}APP
+            DISPLAY_NAME "${PLUGIN_NAME} Standalone"
+            INSTALL_TYPES Full
+        )
+    endif()
 
-    install(PROGRAMS ${APP_PATH}
-        DESTINATION ${APP_INSTALL_DIRECTORY}
-        COMPONENT ${target}APP
-    )
-
-    set(CPACK_COMPONENTS_ALL ${target}APP ${target}VST3)
     set(CPACK_COMPONENT_NAME "${target}")
 
     # Mac installer
     if(APPLE)
-        include(CPackComponent)
-
         configure_file("${CMAKE_SOURCE_DIR}/cmake/component.plist.in" "component.plist" @ONLY)
-        cpack_add_component(${target}APP PLIST "${CMAKE_CURRENT_BINARY_DIR}/component.plist")
+        cpack_add_component(${target}APP 
+            DISPLAY_NAME "${PLUGIN_NAME} Standalone"
+            PLIST "${CMAKE_CURRENT_BINARY_DIR}/component.plist"
+        )
         install(TARGETS ${target}_AU
             DESTINATION "Library/Audio/Plug-Ins/Components"
             COMPONENT ${target}AU
         )
         list(APPEND CPACK_COMPONENTS_ALL ${target}AU)
 
+        cpack_add_component(${target}AU
+            DISPLAY_NAME "${PLUGIN_NAME} AU"
+            INSTALL_TYPES Full
+        )
+
         set(CPACK_PACKAGING_INSTALL_PREFIX "/")
         set(CPACK_GENERATOR "productbuild")
-
+        
     # Windows installer
     elseif(WIN32)
         set(CPACK_GENERATOR "INNOSETUP")
         set(CPACK_INNOSETUP_USE_MODERN_WIZARD ON)
         set(CPACK_INNOSETUP_IGNORE_LICENSE_PAGE ON)
-                
-        set(CPACK_PACKAGE_ICON ${INSTALL_IMAGE})
-        set(CPACK_INNOSETUP_SETUP_WizardImageFile ${INSTALL_IMAGE})
+        set(CPACK_INNOSETUP_SETUP_WizardStyle dark)
+        set(CPACK_INNOSETUP_SETUP_WizardBackImageFile ${INSTALL_IMAGE})
         set(CPACK_INNOSETUP_SETUP_DisableWelcomePage NO)
         set(CPACK_INNOSETUP_SETUP_SetupIconFile "${CMAKE_SOURCE_DIR}/resources/assets/logo_transparent.ico")
-
-        
-        include(CPackComponent)
-
         set(CPACK_INNOSETUP_${target}VST3_INSTALL_DIRECTORY "{commoncf64}/VST3")
 
         cpack_add_install_type(Full DISPLAY_NAME "Full installation")
-
-        cpack_add_component(${target}APP
-            DISPLAY_NAME "${PLUGIN_NAME} Standalone"
-            INSTALL_TYPES Full
-        )
-        cpack_add_component(${target}VST3
-            DISPLAY_NAME "${PLUGIN_NAME} VST3"
-            INSTALL_TYPES Full
-        )
     else()
         set(CPACK_GENERATOR "TGZ")
         configure_file("${CMAKE_SOURCE_DIR}/cmake/install.sh.in" "install.sh" @ONLY)
@@ -102,6 +109,12 @@ function(create_installer target INSTALL_IMAGE)
         # Include the script component in the package
         list(APPEND CPACK_COMPONENTS_ALL ${target}SCRIPT)
     endif()
+
+    # Explicitly set which components to include (prevents other targets' components from being included)
+    set(CPACK_COMPONENTS_ALL ${CPACK_COMPONENTS_ALL})
+    
+    # Install only from this target's subdirectory to avoid installing other targets' components
+    set(CPACK_INSTALL_CMAKE_PROJECTS "${CMAKE_CURRENT_BINARY_DIR};${PROJECT_NAME};ALL;/")
 
     # Linux installer
     include(CPack)
